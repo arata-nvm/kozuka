c98e308c2104b40616d64a0de3c561817743c5c8
arata <arata04962@gmail.com>
2020-08-08 17:05:40 +0900

    :sparkles: implement simple interactive prompt

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use rustyline::error::ReadlineError;
+use rustyline::Editor;
+
 fn main() {
-    println!("Hello, world!");
+    let mut rl = Editor::<()>::new();
+    loop {
+        let input = rl.readline("kozuka> ");
+        match input {
+            Ok(line) => {
+                println!("{}", line);
+            }
+            Err(ReadlineError::Interrupted) | Err(ReadlineError::Eof) => {
+                break;
+            }
+            Err(err) => {
+                println!("Failed to read line: {}", err);
+                break;
+            }
+        }
+    }
 }
