14d469fd802c4eb9110d71394f8e1c44c2bb155f
arata <arata04962@gmail.com>
2020-08-08 17:47:11 +0900

    :sparkles: implement parser

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,22 @@
 use rustyline::error::ReadlineError;
 use rustyline::Editor;
 
+extern crate pest;
+#[macro_use]
+extern crate pest_derive;
+
+use pest::Parser;
+
+#[derive(Parser)]
+#[grammar = "lisp.pest"]
+struct LispParser;
+
 fn main() {
     let mut rl = Editor::<()>::new();
     loop {
         let input = rl.readline("kozuka> ");
         match input {
-            Ok(line) => {
-                println!("{}", line);
-            }
+            Ok(line) => exec(&line),
             Err(ReadlineError::Interrupted) | Err(ReadlineError::Eof) => {
                 break;
             }
@@ -16,6 +24,19 @@ fn main() {
                 println!("Failed to read line: {}", err);
                 break;
             }
+        }
+    }
+}
+
+fn exec(line: &str) {
+    println!("{}", line);
+    match LispParser::parse(Rule::program, &line) {
+        Ok(pairs) => {
+            println!("{:?}", pairs);
+        }
+        Err(err) => {
+            println!("Failed to parse program: {}", err);
+            return;
         }
     }
 }
