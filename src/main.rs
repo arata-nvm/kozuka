8db744071fcb062a8e455a132bd5059299ecc355
arata <arata04962@gmail.com>
2020-08-08 22:17:14 +0900

    :sparkles: implement modulo operation

--- src/main.rs
+++ src/main.rs
@@ -71,6 +71,7 @@ fn eval_op(op: &str, lhs: i32, rhs: i32) -> i32 {
         "-" => lhs - rhs,
         "*" => lhs * rhs,
         "/" => lhs / rhs,
+        "%" => lhs % rhs,
         _ => unreachable!(),
     }
 }
